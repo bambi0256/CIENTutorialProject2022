a0429e400e6fde2ff119c61e5c7098779b25a820
ysshwan147 <chick123159147@gmail.com>
2022-08-02 14:04:53 +0900

    Turret prefab and cannonball prefab

---
+++ Assets/Script/CannonballController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonballController : MonoBehaviour
+{
+    [SerializeField] private float mapWidth;
+    [SerializeField] private float mapHeight;
+    [SerializeField] private float tileLength = 1.0f;
+    [SerializeField] private float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * this.tileLength * this.speed * Time.deltaTime);
+
+        if (Mathf.Abs(transform.position.x) > this.mapWidth + 1 || Mathf.Abs(transform.position.y) > this.mapHeight + 1 )
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+
+        Debug.Log("col");
+    }
+}
