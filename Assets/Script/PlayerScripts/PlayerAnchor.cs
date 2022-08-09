c327edcf10d44e4dbfd592457ae47784cfa2bc91
bambi0256 <bambi0256@gmail.com>
2022-08-09 20:03:45 +0900

    타일 설치 및 길 방향 설정 코드, 타일 자동 변경 기능 개발 중, 타일 자동 변경 기능 오류 해결 중

--- Assets/Script/PlayerScripts/PlayerAnchor.cs
+++ Assets/Script/PlayerScripts/PlayerAnchor.cs
@@ -1,11 +1,12 @@
-using PlayerScripts;
+using System;
 using UnityEngine;
 
-namespace Script.PlayerScripts
+namespace PlayerScripts
 {
     public class PlayerAnchor : MonoBehaviour
     {
-        [SerializeField] private bool isCannonball = false;
+        [SerializeField] private bool isCannonball;
+        [SerializeField] private bool isTileOn;
         private GameObject parent;
 
         // Start is called before the first frame update
@@ -16,11 +17,21 @@ namespace Script.PlayerScripts
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.gameObject.CompareTag("Cannonball"))
+            if (other.gameObject.CompareTag("Tile"))
             {
-                this.isCannonball = true;
+                isTileOn = true;
+            }
+            else if (!other.gameObject.CompareTag("Cannonball")) return;
+            isCannonball = true;
 
-                parent.GetComponent<PlayerController>().getShot();
+            parent.GetComponent<PlayerController>().getShot();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if(other.gameObject.CompareTag("Tile"))
+            {
+                isTileOn = false;
             }
         }
     }
