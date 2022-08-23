9d15e4fe8fbed0dc32d4588c9a05074a71b341b3
bambi0256 <bambi0256@gmail.com>
2022-08-23 14:42:03 +0900

    병합 준비

--- Assets/Script/TileScripts/StartTile.cs
+++ Assets/Script/TileScripts/StartTile.cs
@@ -5,21 +5,6 @@ namespace TileScripts
 {
     public class StartTile : MonoBehaviour
     {
-        public GameObject Ball;
-        public int Direction;
-        public float DelayTime;
-
-        private void Awake()
-        {
-            var position = transform.position;
-            var BallPos = new Vector3(position.x, position.y, position.z - 1);
-            Instantiate(Ball, BallPos, Quaternion.identity);
-            Invoke(nameof(SetBallDir), DelayTime);
-        }
-
-        private void SetBallDir()
-        {
-            BallMove.BallDir = Direction;
-        }
+        
     }
 }
