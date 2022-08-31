abf1856af9d134dc88e2de11b60fc00eda9d61f2
ysshwan147 <chick123159147@gmail.com>
2022-09-01 05:42:59 +0900

    스위치방벽 스프라이트 적용, 맵 일부 수정, 꺽인선 제외 전선 스프라이트 적용

--- Assets/Script/ObjectScripts/BarrierCircuit.cs
+++ Assets/Script/ObjectScripts/BarrierCircuit.cs
@@ -2,25 +2,19 @@ using UnityEngine;
 
 namespace ObjectScripts
 {
-    public class SwitchBarrier : MonoBehaviour
+    public class BarrierCircuit : MonoBehaviour
     {
         [SerializeField] private bool switchOn;
-        private int defaultLayer;
 
         [SerializeField] private Sprite offSprite;
         [SerializeField] private Sprite onSprite;
         private SpriteRenderer spriteRenderer;
 
-        private BarrierCircuit barrierCircuitScript;
-
         // Start is called before the first frame update
         void Start()
         {
             this.spriteRenderer = GetComponent<SpriteRenderer>();
-            defaultLayer = gameObject.layer;
             setSprite();
-
-            this.barrierCircuitScript = transform.GetChild(0).GetComponent<BarrierCircuit>();
         }
 
 
@@ -33,23 +27,10 @@ namespace ObjectScripts
         }
 
 
-        private void toggleLayer()
-        {
-            // layer 8 is Obstruction
-            if (this.switchOn)
-                gameObject.layer = 8;
-            else
-                gameObject.layer = defaultLayer;
-
-        }
-
-
         public void setSwitchOn(bool switchState)
         {
             this.switchOn = switchState;
             setSprite();
-            this.barrierCircuitScript.setSwitchOn(this.switchOn);
-            toggleLayer();
         }
     }
 }
