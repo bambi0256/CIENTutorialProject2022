41f179c33bfa27ad6957966deec1cc1a482b2b67
서동민 <sdm0204@naver.com>
2022-09-04 22:36:26 +0900

    UI
    -디자인 수정
    -설명 말풍선 파괴시간 설정
    등등

--- Assets/Script/UIScripts/StageManager.cs
+++ Assets/Script/UIScripts/StageManager.cs
@@ -8,10 +8,8 @@ public class StageManager : MonoBehaviour
     [SerializeField] GameObject[] NextStages;
     [SerializeField] GameObject ClearUI;
     [SerializeField] GameObject[] BubbleUI;
-    int currentStage = 0;
-
-    [SerializeField] Rigidbody PlayerRigid;
-    [SerializeField] GameObject[] ForDestroy;
+    [SerializeField] GameObject[] SetBack;
+    int currentStage= 0;
 
     public void ShowClearUI()
     {
@@ -21,26 +19,109 @@ public class StageManager : MonoBehaviour
 
     public void NextStageBtn()
     {
-     NextStages[currentStage++].SetActive(false);
-     NextStages[currentStage].SetActive(true);
-     ClearUI.SetActive(false);
-        if (NextStages[currentStage] == NextStages[3])
+        for (int i = 0; i < NextStages.Length; i++)
+        {
+            if (NextStages[i].activeSelf == true) { currentStage = i; break; }
+        }
+
+        NextStages[currentStage++].SetActive(false);
+        NextStages[currentStage].SetActive(true);
+        SetStageBack();
+
+        ClearUI.SetActive(false);
+
+
+        if (NextStages[currentStage] == NextStages[0])
+        {
+            BubbleUI[0].SetActive(true);
+
+            Invoke("DestroyInfo", 4f);
+        }
+        else if (NextStages[currentStage] == NextStages[3])
         {
             BubbleUI[1].SetActive(true);
+            Invoke("DestroyInfo", 18f);
         }
         else if (NextStages[currentStage] == NextStages[4])
         {
             BubbleUI[2].SetActive(true);
+            Invoke("DestroyInfo", 15f);
         }
         else if (NextStages[currentStage] == NextStages[6])
         {
             BubbleUI[3].SetActive(true);
+            Invoke("DestroyInfo", 10f);
         }
-        else if (NextStages[currentStage] == NextStages[7])
+        /*else if (NextStages[currentStage] == NextStages[7])
         {
             BubbleUI[4].SetActive(true);
+            Invoke("DestroyInfo", 15f);
+        }*/
+    }
+    public void SetStageBack()
+    {
+        if (NextStages[currentStage] == NextStages[3])
+        {
+            SetBack[0].SetActive(false);
+            SetBack[1].SetActive(true);
+        }
+        if (NextStages[currentStage] == NextStages[6])
+        {
+            SetBack[1].SetActive(false);
+            SetBack[2].SetActive(true);
+        }
+        if (NextStages[currentStage] == NextStages[9])
+        {
+            SetBack[2].SetActive(false);
+            SetBack[3].SetActive(true);
         }
     }
+    public void DestroyInfo()
+    {
+        for (int k = 0; k < 5; k++)
+            BubbleUI[k].SetActive(false);
+    }
+
+    public void SelectStage()
+    {
+
+        for (int i = 0; i < NextStages.Length; i++)
+        {
+            if (NextStages[i].activeSelf == true) { currentStage = i; break; }
+        }
+        SetStageBack();
+
+        if (NextStages[currentStage] == NextStages[0])
+        {
+            BubbleUI[0].SetActive(true);
+
+            Invoke("DestroyInfo", 4f);
+        }
+        else if (NextStages[currentStage] == NextStages[3])
+        {
+            BubbleUI[1].SetActive(true);
+            Invoke("DestroyInfo", 18f);
+        }
+        else if (NextStages[currentStage] == NextStages[4])
+        {
+            BubbleUI[2].SetActive(true);
+            Invoke("DestroyInfo", 15f);
+        }
+        else if (NextStages[currentStage] == NextStages[6])
+        {
+            BubbleUI[3].SetActive(true);
+            Invoke("DestroyInfo", 10f);
+        }
+        /*else if (NextStages[currentStage] == NextStages[7])
+        {
+            BubbleUI[4].SetActive(true);
+            Invoke("DestroyInfo", 15f);
+        }
+        */
+
+    }
+
+
     public void QuitBtn()
     {
         NextStages[currentStage].SetActive(false);
@@ -48,34 +129,25 @@ public class StageManager : MonoBehaviour
     public void restartBtn()
     {
 
-        Reset();
 
         NextStages[currentStage].SetActive(false);
         NextStages[currentStage].SetActive(true);
 
     }
-
-    public void Reset()
+    public void Level1()
     {
-     
-        ForDestroy[0] = transform.Find("Player").gameObject;
-        ForDestroy[1] = GameObject.Find("RoadTile(Clone)");
-        ForDestroy[2] = GameObject.Find("Ball(Clone)");
-        for (int i = 0; i < 3; i++)
-        {
-            Destroy(ForDestroy[i]);
-        }
+        Invoke("DestroyInfo", 5f);
     }
+
+
 
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-       
     }
 }
